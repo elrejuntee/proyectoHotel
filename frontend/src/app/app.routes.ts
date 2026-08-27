4615982d0a629e15d761a05168f8ab3ca222c599
Alexis Lazarte <alexis_lazarte@hotmail.com>
2026-08-27 13:10:36 -0300

    feat: Se creo la carpeta servcios con sus componentes, mas el ruteo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'servicios', component: ServiciosComponent }
 ];
