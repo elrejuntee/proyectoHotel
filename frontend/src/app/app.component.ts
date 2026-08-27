297e4316111f74a074fcba147fa8bde0836e2734
Alexis Lazarte <alexis_lazarte@hotmail.com>
2026-08-27 12:44:02 -0300

    feat: agregar componentes navbar, footer y home

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar';
+import { FooterComponent } from './components/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
