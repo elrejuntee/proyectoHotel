8a6f9615c4767d3db58dfd4f1cb0be3b090655d0
Mariel Emilse Jimenez <emilsejimenez@gmail.com>
2026-08-29 14:36:22 -0300

    feat: se agrega ruta para el componente Quienes-somos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { ServiciosComponent } from './components/servicios/servicios.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'servicios', component: ServiciosComponent }
+    { path: 'servicios', component: ServiciosComponent },
+    { path: 'quienes-somos', component: QuienesSomosComponent},
 ];
