d41005d726e4b6123adb09b585c3255c5200ea4d
adriel1364 <adridelosanto@gmail.com>
2026-08-30 04:07:38 -0300

    feat(frontend): crea el componente resultados-busqueda
    - Agrega la estructura del layout de filtros y listado de habitaciones.
    
    - Configura la visualización de tarjetas y opciones de ordenamiento.

---
+++ frontend/src/app/components/resultados-busqueda/resultados-busqueda.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados-busqueda',
+  standalone: true,
+  imports: [],
+  templateUrl: './resultados-busqueda.html',
+  styleUrl: './resultados-busqueda.css'
+})
+export class ResultadosBusquedaComponent {
+  // Acá irá la lógica del componente más adelante
+}
