0eb6360270c04786c34e70922e4336f8527ddf01
Fernando Agustin Perez <aguss.97@outlook.com>
2026-08-30 23:09:14 -0300

    feat: agregar formulario para los servicios

---
+++ frontend/src/app/components/formulario-servicios/formulario-servicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-servicios',
+  imports: [],
+  templateUrl: './formulario-servicios.component.html',
+  styleUrl: './formulario-servicios.component.css'
+})
+export class FormularioServiciosComponent {
+
+}
