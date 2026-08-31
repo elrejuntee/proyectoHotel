e0a6bdbe80617e59cb88c07b2b2d63196f5b5eff
Fernando Agustin Perez <aguss.97@outlook.com>
2026-08-30 22:52:02 -0300

    feat: agregar formulario de registro

---
+++ frontend/src/app/auth/registro/registro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarAuthComponent } from '../navbar-auth/navbar-auth.component';
+
+@Component({
+  selector: 'app-registro',
+  imports: [NavbarAuthComponent],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+
+}
