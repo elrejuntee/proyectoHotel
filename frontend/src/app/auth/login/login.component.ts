d04eff674c2d1568cd067fcbdad771179d0f89ea
Fernando Agustin Perez <aguss.97@outlook.com>
2026-09-18 17:36:37 -0300

    fix: corregir rutas de navegacion en los botones de ingresar segun el rol del formulario login

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { NavbarAuthComponent } from '../navbar-auth/navbar-auth.component';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [NavbarAuthComponent, ReactiveFormsModule],
+  imports: [NavbarAuthComponent, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
