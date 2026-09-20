48a53112366e0c0f48d1e1c0301527db0bfa573e
Mariel Emilse Jimenez <emilsejimenez@gmail.com>
2026-09-19 22:41:24 -0300

    refactor: se modifica el nombre de la habitacion usando tipos_habitacion

--- frontend/src/app/components/resultados-busqueda/resultados-busqueda.component.ts
+++ frontend/src/app/components/resultados-busqueda/resultados-busqueda.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HabitacionesService, HabitacionApi } from '../../services/habitaciones/habitaciones.service';
+import { HabitacionesService, HabitacionApi, TipoHabitacionApi } from '../../services/habitaciones/habitaciones.service';
 
 @Component({
   selector: 'app-resultados-busqueda',
@@ -11,19 +11,35 @@ import { HabitacionesService, HabitacionApi } from '../../services/habitaciones/
 })
 export class ResultadosBusquedaComponent {
   habitaciones: HabitacionApi[] = [];
+  habitacionesOriginales: HabitacionApi[] = [];
+  tiposHabitacion: TipoHabitacionApi[] = [];
   cargando: boolean = true;
   mensajeError: string = '';
 
   constructor(private habitacionesService: HabitacionesService) {}
 
   ngOnInit(): void {
-    this.obtenerHabitaciones();
+    this.cargarDatosIniciales();
   }
 
+  cargarDatosIniciales(): void {
+    this.cargando = true;
+        this.habitacionesService.obtenerTiposHabitacion().subscribe({
+      next: (tipos) => {
+        this.tiposHabitacion = tipos;
+        this.obtenerHabitaciones(); // Una vez obtenidos los tipos, traemos las habitaciones
+      },
+      error: (error) => {
+        console.error('Error al obtener los tipos', error);
+        this.obtenerHabitaciones(); // Intentamos traer las habitaciones igual
+      }
+    });
+  }
   obtenerHabitaciones(): void{
     this.cargando = true;
     this.habitacionesService.obtenerHabitaciones().subscribe({
       next: (data) => {
+        this.habitacionesOriginales = data;
         this.habitaciones = data;
         this.cargando = false;
         console.log('Habitaciones disponibles', this.habitaciones)
@@ -34,5 +50,20 @@ export class ResultadosBusquedaComponent {
         this.cargando = false;
         }
     })
+  }
+  filtrarPorPrecio(valorInput: string) {
+    const precioMaximo = Number(valorInput);
+
+    if (!precioMaximo || precioMaximo <= 0) {
+      this.habitaciones = [...this.habitacionesOriginales];
+    } else {
+      this.habitaciones = this.habitacionesOriginales.filter(
+        h => h.precio <= precioMaximo
+      );
+    }
+  }
+  obtenerNombreTipo(idTipo: number | string): string {
+    const tipoEncontrado = this.tiposHabitacion.find(t => t.id == idTipo);
+    return tipoEncontrado ? tipoEncontrado.nombre : 'Habitación';
   }
 }
