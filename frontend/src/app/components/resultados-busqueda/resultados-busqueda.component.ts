846ad05fc9238ac90dc3d34f46bc4405563ce76d
Alexis Lazarte <alexis_lazarte@hotmail.com>
2026-09-20 00:20:00 -0300

    feat: enlazar boton Ver habitacion con detalle-habitacion

--- frontend/src/app/components/resultados-busqueda/resultados-busqueda.component.ts
+++ frontend/src/app/components/resultados-busqueda/resultados-busqueda.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-resultados-busqueda',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './resultados-busqueda.component.html',
   styleUrl: './resultados-busqueda.component.css'
 })
