fa95a81daa1957f1873ecc5367ef8b25cb578c31
Alexis Lazarte <alexis_lazarte@hotmail.com>
2026-09-21 00:21:55 -0300

    Se crearon los componentes pago confirmado

---
+++ frontend/src/app/components/pago-confirmado/pago-confirmado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-confirmado',
+  imports: [],
+  templateUrl: './pago-confirmado.component.html',
+  styleUrl: './pago-confirmado.component.css'
+})
+export class PagoConfirmadoComponent {
+
+}
